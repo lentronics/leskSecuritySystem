898847fa85bb71d3e1d8f1075b54b2f60878ca4f
Pedro Costa <me@iampedrocosta.com>
2016-07-30 00:27:57 +0000

    secsys_2 New Python script to upload video to IAMPC Web API via REST

---
+++ rest_request.py
@@ -0,0 +1,24 @@
+// Created by Pedro Costa
+// Based on Quickstart Requests Tutorial
+// http://docs.python-requests.org/en/latest/user/quickstart/
+
+import requests
+import json
+
+url = 'http://iampedrocosta.com/iampcwebapi/';
+
+//reqData = '{"video":"somevideo", "recorded_date":"dd/mm/YYYY hh:ii:ss", "videosize":"879kb"}'
+
+fileData = {'file': ('video.mp4', read('video.mp4'), 'media/vidapp-mp4', {'Expires': 0})}
+
+//response = requests.post(url, data=json.dumps(reqData))
+response = requests.post(url, files=fileData)
+
+if(response.raise_for_status() == ""){
+// video has been uploaded
+r.text
+}else{
+// error when uploading the video through IAMPC Web API
+r.text
+}
+
